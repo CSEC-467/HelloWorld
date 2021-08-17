d697778809b0ef0fc28f2350824742f112a63945
Rob <rboics@rit.edu>
2021-08-17 17:32:29 -0400

    Added sample code indicating how to print messages to logcat and generate a toast popup.

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button h= (Button)findViewById(R.id.btnHello);
+        OnClickListener O  = new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Debug Message", "Printing to logcat");
+                Toast.makeText(v.getContext(), "Hello World", Toast.LENGTH_LONG).show();
+            }
+        };
+        h.setOnClickListener(O);
     }
 }
